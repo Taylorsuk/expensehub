80488bc7b5e916a4c2dc696de61731be2b9629d2
Simon <simon@elementdev.co.uk>
2020-08-29 22:10:35 +0100

    add iOS and Android native apps and remove unused components

--- src/app/modals/add-expense/add-expense.module.ts
+++ src/app/modals/add-expense/add-expense.module.ts
@@ -3,19 +3,11 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { AddExpensePageRoutingModule } from './add-expense-routing.module';
 import { AddExpensePage } from './add-expense.page';
-import { SharedComponentsModule } from "../../components/components.module";
 import { ReactiveFormsModule } from '@angular/forms';
-import { NgxCurrencyModule } from "ngx-currency";
+import { NgxCurrencyModule } from 'ngx-currency';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    IonicModule,
-    AddExpensePageRoutingModule,
-    SharedComponentsModule,
-    ReactiveFormsModule,
-    NgxCurrencyModule,
-  ],
-  declarations: [AddExpensePage]
+  imports: [CommonModule, IonicModule, AddExpensePageRoutingModule, ReactiveFormsModule, NgxCurrencyModule],
+  declarations: [AddExpensePage],
 })
-export class AddExpensePageModule { }
+export class AddExpensePageModule {}
